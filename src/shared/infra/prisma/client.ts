36dc58720fa92204d9c9221848a2aee497abd094
uuuuuuuuu0 <pedrovrs@usp.br>
2022-11-23 03:12:16 -0300

    Changing PrismaClient variable name in client.ts

--- src/shared/infra/prisma/client.ts
+++ src/shared/infra/prisma/client.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line quotes
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const client = new PrismaClient();
 
-export default prisma;
+export default client;
