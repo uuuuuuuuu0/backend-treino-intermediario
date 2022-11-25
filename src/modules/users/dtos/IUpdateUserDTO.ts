cc1727d3e20b2ae7e7f687022e207b9f8fa7d4cd
uuuuuuuuu0 <pedrovrs@usp.br>
2022-11-25 07:19:04 -0300

    IUpdateUserDTO now has all optional inputs

--- src/modules/users/dtos/IUpdateUserDTO.ts
+++ src/modules/users/dtos/IUpdateUserDTO.ts
@@ -1,8 +1,8 @@
 interface IUpdateUserDTO {
-    name: string;
-    cpf: number;
-    email: string;
-    password: string;
+    name?: string;
+    cpf?: number;
+    email?: string;
+    password?: string;
 }
 
 export default IUpdateUserDTO;
