748487be9204f8f3ccc705e981e2065352d63fbc
uuuuuuuuu0 <pedrovrs@usp.br>
2022-11-25 07:33:06 -0300

    Minor autofixes

--- src/config/swagger/index.ts
+++ src/config/swagger/index.ts
@@ -1,19 +1,19 @@
-import { OpenAPIV3 } from "openapi-types";
+import { OpenAPIV3 } from 'openapi-types';
 
 const swagger: OpenAPIV3.Document = {
-  openapi: "3.0.0",
+  openapi: '3.0.0',
   info: {
-    title: "API treinamento",
-    description: "Documentação",
+    title: 'API treinamento',
+    description: 'Documentação',
     contact: {
-      email: "pedro.souza@polijunior.com.br",
+      email: 'pedro.souza@polijunior.com.br',
     },
-    version: "1.0.0",
+    version: '1.0.0',
   },
   servers: [
     {
-      url: "http://localhost:3333",
-      description: "Local server",
+      url: 'http://localhost:3333',
+      description: 'Local server',
     },
   ],
   paths: {},
