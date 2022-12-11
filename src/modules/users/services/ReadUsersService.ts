6dc22823707a7a3af896108aae0f1509d8d71e3c
uuuuuuuuu0 <pedrovrs@usp.br>
2022-12-11 08:58:33 -0300

    Read now does not return encoded password

--- src/modules/users/services/ReadUsersService.ts
+++ src/modules/users/services/ReadUsersService.ts
@@ -5,13 +5,20 @@ import IUserRepository from '../repositories/IUsersRepository';
 @injectable()
 export default class ReadUsersService {
   constructor(
-        @inject('UsersRepository')
-        private usersRepository: IUserRepository,
+    @inject('UsersRepository')
+    private usersRepository: IUserRepository,
   ) { }
 
-  public async execute(): Promise<User[]> {
+  public async execute(): Promise<Omit<User, 'password'>[]> {
     const users = await this.usersRepository.readUsers();
 
-    return users;
+    const usersWithoutPassword: Omit<User, 'password'>[] = [];
+
+    users.forEach((element) => {
+      const { password: _, ...userWithoutPassword } = element;
+      usersWithoutPassword.push(userWithoutPassword);
+    });
+
+    return usersWithoutPassword;
   }
 }
